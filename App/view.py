a7d0d0ef785230a1b08625c98b9e039d7e03edd5
juancamilobonet2 <j.bonet@uniandes.edu.co>
2021-02-17 22:08:27 -0500

    paso 9

--- App/view.py
+++ App/view.py
@@ -34,10 +34,22 @@ se hace la solicitud al controlador para ejecutar la
 operación solicitada
 """
 
+
+def printLine():
+    print("---------------------------------------------------------------")
+
 def printMenu():
-    print("Bienvenido")
+    printLine()
+    print("Menu")
     print("1- Cargar información en el catálogo")
-    print("2- ")
+    print("2- Consultar los videos con más views por país y categoría")
+    print("3- Consultar el video que más dias ha sido trending por país")
+    print("4- Consultar el video que más dias ha sido trending por categoría")
+    print("5- Consultar el video con más likes por tag")
+    print("0- Salir")
+    printLine()
+
+
 
 catalog = None
 
@@ -48,11 +60,48 @@ while True:
     printMenu()
     inputs = input('Seleccione una opción para continuar\n')
     if int(inputs[0]) == 1:
+        #Cargar archivos
+
+        file_name = input("Nombre del archivo:")
         print("Cargando información de los archivos ....")
+        catalog = controller.initCatalog()
+        catalog = controller.loadData(catalog)
+        print(catalog)
+        print("Done")
+
 
     elif int(inputs[0]) == 2:
-        pass
+        #Req 1
+
+        videos = int(input("Cuantos videos: "))
+        country = int(input("País: "))
+        category = int(input("Categoría: "))
+        print("Cargando...")
+        #function bla bla
+
+    elif int(inputs[0]) == 3:
+        #Req 2
+
+        country = int(input("País: "))
+        print("Cargando...")
+        #funcion aqui
+
+    elif int(inputs[0]) == 4:
+        #Req 3
+
+        category = int(input("Categoría: "))
+        print("Cargando...")
+        #funcion aqui
+
+    elif int(inputs[0]) == 5:
+        #Req 4
+
+        videos = int(input("Cuantos videos: "))
+        tag = input("Tag: ")
+        print("Cargando...")
+        #funcion aqui
 
     else:
+        print("Adios!")
         sys.exit(0)
 sys.exit(0)
